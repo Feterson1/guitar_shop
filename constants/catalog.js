544f82c185f203f1620229f1784831696d2fd204
Feterson1 <Feterson@bk.ru>
2023-02-03 00:32:16 +0600

    project structure

---
+++ constants/catalog.js
@@ -0,0 +1,34 @@
+const CATALOG = [
+    {
+        id: 'el1',
+        name: '',
+        img: '',
+        price: 0,
+    },
+    {
+        id: 'el2',
+        name: '',
+        img: '',
+        price: 0,
+    },
+    {
+        id: 'el3',
+        name: '',
+        img: '',
+        price: 0,
+    },
+    {
+        id: 'el4',
+        name: '',
+        img: '',
+        price: 0,
+    },
+    {
+        id: 'el5',
+        name: '',
+        img: '',
+        price: 0,
+    },
+    
+    
+]
